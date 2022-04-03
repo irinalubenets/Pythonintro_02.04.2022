9c3f6de4a8a4a4fcbc947038a4c3fac6479b39cf
Irina Lubenets <zavorueva@gmail.com>
2022-04-03 16:43:40 +0200

    калькулятор

---
+++ lesson_3.py
@@ -0,0 +1,19 @@
+input_case = input("Выбери тип операции:\n 1 +\n 2 -\n 3 *\n 4 /\n")
+try:
+    print("Примечание - Дробные числа вводятся через точку")
+    value_1 = float(input("Введи первое число:"))
+    value_2 = float(input("Введи второе число:"))
+    if input_case == "1":
+        print("Результат = ", value_1 + value_2)
+    elif input_case == "2":
+        print("Результат = ", value_1 - value_2)
+    elif input_case == "3":
+        print("Результат = ", value_1 * value_2)
+    elif input_case == "4":
+        print("Результат = ", value_1 / value_2)
+    else:
+        print("Неверный тип операции")
+except ValueError:
+    print("Вы ввели не число")
+except ZeroDivisionError:
+    print("Делить на ноль нельзя")
